a139cb47b12eac64743a29e5917d4617968a4c85
monikatrznadel <monikatrznadel@users.noreply.github.com>
2020-12-05 13:33:07 +0000

    Zadani13

---
+++ zadanie13.py
@@ -0,0 +1,9 @@
+Print('Zadanie 13')
+
+ranking = {'name': 'Player1', 'pkt': '10', 'kat': 'junior'}
+
+print(ranking['name'])
+print(ranking['kat'])
+
+for i in ranking:
+    print("{0}:{1}".format(i, ranking[i]))
