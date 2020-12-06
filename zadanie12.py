497bf81db3ddffe27d8cf5a96af0752e98ed8e90
monikatrznadel <monikatrznadel@users.noreply.github.com>
2020-12-06 08:05:26 +0000

    Poprawki wizualne

--- zadanie12.py
+++ zadanie12.py
@@ -1,11 +1,13 @@
 kosz_cena=[100, 100, 90, 100, 100]
 kosz_prod=['mleko','jajka','majonez','seler','jablko']
 
+print('Wyjsciowa wartosc koszyka to: ')
 suma = 0
 for i in kosz_cena:
     suma=suma + i
 print('Wartosc koszyka',suma)
 ## Dodatkowe 1
+print('Dodatkowe 1: ')
 if len(kosz_prod) > 3:
     suma_dot1 = suma*0.95
     print('cena kosza1 po znizce 5 % to ', suma_dot1)
@@ -15,6 +17,7 @@ elif suma>500:
 else:
     print('Brak znizek, cena koszyka1', suma)
 ## dodatkowe 2
+print('Dodatkowe 2: ')
 if suma>500:
     suma_dot1 = suma*0.9
     print ('cena kosza2 po znizce 10 % to', suma_dot1)
@@ -25,6 +28,7 @@ else:
     print('Brak znizek, cena koszyka2', suma)
 
 ## dodatkowe3
+print('Dodatkowe 3: ')
 min=kosz_cena[0]
 for i in kosz_cena:
     if i<min:
@@ -33,6 +37,7 @@ suma_d3 = suma - min
 print('Suma koszyka gdzie najtanszy produkt jest gratis to ',suma_d3)
 
 ##dodatkowe 4
+print('Dodatkowe 4: ')
 suma_d4 = 0
 for idx in range(len(kosz_cena)):
     if (idx+1) % 3 != 0:
